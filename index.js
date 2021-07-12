b0ad92f8c92826a90a8783cdf3d9096caae6dab0
Madnesssik <78844616+Madnesssik@users.noreply.github.com>
2021-07-12 18:57:19 +0200

    fix

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 module.exports = class Embed {
     constructor(title, description, footer, timestamp, color) {
-        title = this.title
-        description = this.description
-        footer = this.footer
-        timestamp = this.timestamp
-        color = this.color
+      title = this.title
+      description = this.description
+      footer = this.footer
+      timestamp = this.timestamp
+      color = this.color
     }
-    new(title, description, footer, timestamp, color) {
+    make(title, description, footer, timestamp, color) {
         const Discord = require('discord.js');
-        var version = Discord.version.split('');
-        if(version.includes('(')) {
-            version = version.join('').split('(').pop().split('');
-        }
-        version = parseInt(version[0] + version[1]);
 
         if(!title) throw new Error(`title must be provided`)
         if(!description) throw new Error(`description must be provided`)
@@ -20,35 +15,17 @@ module.exports = class Embed {
         if(!timestamp) throw new Error(`timestamp must be provided`)
         if(!color) throw new Error(`The color must be provided`)
 
-        if (version === 12 || version === 13) {
+        if(timestamp === false) return new Discord.MessageEmbed()
+            .setTitle(title)
+            .setDescription(description)
+            .setFooter(footer)
+            .setColor(color);
 
-            if(timestamp === false) return new Discord.MessageEmbed()
-                .setTitle(title)
-                .setDescription(description)
-                .setFooter(footer)
-                .setColor(color);
-
-            else if(timestamp === true) return new Discord.MessageEmbed()
-                .setTitle(title)
-                .setDescription(description)
-                .setFooter(footer)
-                .setColor(color)
-                .setTimestamp(); 
-        } else if (version === 11) {
-
-            if(timestamp === false) return new Discord.RichEmbed()
-                .setTitle(title)
-                .setDescription(description)
-                .setFooter(footer)
-                .setColor(color);
-
-            else if(timestamp === true) return new Discord.RichEmbed()
-                .setTitle(title)
-                .setDescription(description)
-                .setFooter(footer)
-                .setColor(color)
-                .setTimestamp(); 
-
-        }
+        else return new Discord.MessageEmbed()
+            .setTitle(title)
+            .setDescription(description)
+            .setFooter(footer)
+            .setColor(color)
+            .setTimestamp(); 
     }
 }
